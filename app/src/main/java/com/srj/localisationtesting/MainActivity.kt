2d44eae29d99af8b637ef704f653e01351833bfe
srjranjan <sudhanshuwithyoualll@gmail.com>
2022-01-10 20:26:01 +0530

    added dark mode and localisation

--- app/src/main/java/com/srj/localisationtesting/MainActivity.kt
+++ app/src/main/java/com/srj/localisationtesting/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.srj.localisationtesting
 
+import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +9,7 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import com.srj.localisationtesting.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -19,26 +22,58 @@ class MainActivity : AppCompatActivity() {
 
         when (this.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
             Configuration.UI_MODE_NIGHT_YES -> {
-                binding.switch1.text = getString(R.string.lightmode)
+
+                binding.btnThemeChange.setImageResource(R.drawable.ic_sun_warm)
                 window.statusBarColor = getColor(R.color.black)
-                binding.switch1.setOnClickListener {
-                    if (!binding.switch1.isChecked) AppCompatDelegate.setDefaultNightMode(
-                        MODE_NIGHT_YES
-                    ) else AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+                binding.btnThemeChange.setOnClickListener {
+                    AppCompatDelegate.setDefaultNightMode(
+                        MODE_NIGHT_NO
+                    )
+                    binding.btnThemeChange.setImageResource(R.drawable.ic_night)
                 }
             }
             Configuration.UI_MODE_NIGHT_NO -> {
-                binding.switch1.text = getString(R.string.darkmode)
+                binding.btnThemeChange.setImageResource(R.drawable.ic_night)
                 window.statusBarColor = getColor(R.color.purple_500)
 
-                binding.switch1.setOnClickListener {
-                    if (binding.switch1.isChecked) AppCompatDelegate.setDefaultNightMode(
-                        MODE_NIGHT_NO
-                    ) else AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+                binding.btnThemeChange.setOnClickListener {
+                    AppCompatDelegate.setDefaultNightMode(
+                        MODE_NIGHT_YES
+                    )
+                    binding.btnThemeChange.setImageResource(R.drawable.ic_sun_warm)
                 }
             }
         }
 
+        binding.button.setOnClickListener {
+            changeLang(this, "hi")
+            this.finish()
+
+        }
+        binding.button2.setOnClickListener {
+            changeLang(this, "bn")
+            this.finish()
+
+        }
+        binding.button3.setOnClickListener {
+            changeLang(this, "pa")
+            this.finish()
+
+        }
+    }
+
+    private fun changeLang(context: Context, lang: String) {
+        val locale = Locale(lang)
+        Locale.setDefault(locale)
+        val dm = resources.displayMetrics
+        val configuration = context.resources.configuration
+        configuration.setLocale(locale)
+        configuration.setLayoutDirection(locale)
+        resources.updateConfiguration(configuration, dm)
+
+        startActivity(Intent(this, MainActivity::class.java))
 
     }
+
+
 }
